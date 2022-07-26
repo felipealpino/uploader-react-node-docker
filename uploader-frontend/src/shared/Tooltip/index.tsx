ade7f2028ef5ff1eb27d70795fa78b28e948b447
felipealpino <felipealpino1@gmail.com>
2022-07-26 14:58:19 -0300

    Tooltip component

---
+++ uploader-frontend/src/shared/Tooltip/index.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties, useCallback, useRef, useState } from 'react';
+import styles from './styles.module.scss';
+
+export interface ITooltip {
+  text: string;
+  children?: React.ReactNode;
+}
+
+type ITooltipDisplay = 'none' | 'block';
+
+const Tooltip: React.FC<ITooltip> = ({ text, children }) => {
+  const [boxDisplay, setBlockDisplay] = useState<ITooltipDisplay>('none');
+  const tooltipBoxStyles = useRef<CSSProperties>({});
+
+  const handleOnMouseEnter = useCallback((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setBlockDisplay('block');
+    tooltipBoxStyles.current = { ...tooltipBoxStyles.current, top: event.clientY };
+    tooltipBoxStyles.current = { ...tooltipBoxStyles.current, left: event.clientX };
+  }, []);
+
+  console.log(tooltipBoxStyles.current);
+  return (
+    <div className={`${styles['tooltip-container']}`}>
+      <div style={{ ...tooltipBoxStyles.current, display: boxDisplay }} className={`${styles['tooltip-box']}`}>
+        {text}
+      </div>
+      <div onMouseLeave={() => setBlockDisplay('none')} onMouseEnter={event => handleOnMouseEnter(event)}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export { Tooltip };
